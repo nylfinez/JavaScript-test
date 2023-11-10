faf074b15c831bd0599ddb231bd0dfcd96e71005
nyl <127328559+nylfinez@users.noreply.github.com>
2023-11-10 16:30:40 +0100

    Update finez_nyl_test_javascript_algo_ex3.js

--- finez_nyl_test_javascript_algo_ex3.js
+++ finez_nyl_test_javascript_algo_ex3.js
@@ -1,9 +1,24 @@
-//1 use check_array with the to_validate table
-function check_line(to_validate) {
+//1 Check table rows
+function check_row(to_validate) {
     for (let i = 0; i < to_validate.length; i++) {
         let line = to_validate[i];
         if (!check_array(line)){
+            console.error(`Error in row ${i + 1}: ${JSON.stringify(line)}`)
+        }
+    }
+}
+
+//2 Check table columns
+function check_column(to_validate) {
+    const transpose = array => array[0].map( (x, col) => array.map(row => row[col]) ); //function to switch columns and rows
+
+    let transposedTable = transpose(to_validate);
+    for (let i = 0; i < transposedTable.length; i++) {
+        let col = transposedTable[i];
+        if (!check_array(col)){
             console.error(`Error in column ${i + 1}: ${JSON.stringify(line)}`)
         }
     }
 }
+
+//3 check regions
